d48d576b56a41f87624b53f9426a92d7a4f1999d
David E. Perez Negron R <david@nethunters.xyz>
2023-08-08 00:27:21 -0600

    Updates

--- scripts/SmartWallet.ts
+++ scripts/SmartWallet.ts
@@ -1,20 +1,37 @@
 import { ethers } from "hardhat";
 import { EthersAdapter } from '@safe-global/protocol-kit'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-async function main() {
-  // Deploy ERC20Votes Contract
-  const provider = new ethers.AlchemyProvider(network?: Networkish, apiKey?: null | string)(
-      "maticmum",
-      process.env.INFURA_API_KEY
-  );
-  //const provider = new ethers.providers.InfuraProvider(
-  //    "goerli",
-  //    process.env.INFURA_API_KEY
-  //);
+let accounts: SignerWithAddress[];
 
-  const safeOwner = provider.getSigner(0)
+async function main() {
+  await initAccounts();
+  // Setup Provider
+  //const provider = new ethers.JsonRpcProvider(
+  //        process.env.ALCHEMY_BASE_RPC_URL
+  //    );
+  // const provider = new ethers.JsonRpcProvider(
+  //           "HTTP://127.0.0.1:7545"
+  //       );
+
+
+  //console.log({ provider });
+  //const pkey = process.env.PRIVATE_KEY_ACCOUNT;
+  //console.log({ pkey });
+  //const balanceBN = await ethers.provider.getBalance(
+  //  accounts[Number(index)].address
+  //);
+  //const lastBlock = await provider.getBlock("latest");
+  //console.log({ lastBlock });
+  //const wallet = new ethers.Wallet(`${pkey}`);
+  //const signer = wallet.connect(provider);
+
+  const safeOwner = ethers.provider.getSigner(0)
+
+
+  console.log({ safeOwner });
 
   const ethAdapter = new EthersAdapter({
       ethers,
@@ -22,13 +39,10 @@ async function main() {
   })
 
     
-  console.log({ provider });
-  const pkey = process.env.PRIVATE_KEY_ACCOUNT1;
-  console.log({ pkey });
-  const lastBlock = await provider.getBlock("latest");
-  console.log({ lastBlock });
-  const wallet = new ethers.Wallet(`${pkey}`);
-  const signer = wallet.connect(provider);
+}
+
+async function initAccounts() {
+  accounts = await ethers.getSigners();
 }
 
 main().catch((error) => {
