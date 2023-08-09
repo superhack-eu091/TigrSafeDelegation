13506d947057a8c8bdc0919ab048cd323a300b90
David E. Perez Negron R <david@nethunters.xyz>
2023-08-09 03:39:49 -0600

    Adding functional deployment and getting info from protocol kit

--- scripts/SmartWallet.ts
+++ scripts/SmartWallet.ts
@@ -16,10 +16,14 @@ async function main() {
   //const provider = new ethers.providers.AlchemyProvider("optimism-goerli",
   //         process.env.ALCHEMY_API_KEY_OPTIMISM_GOERLI
   //     );
-  // Goerli
-  const provider = new ethers.providers.AlchemyProvider("goerli",
-             process.env.ALCHEMY_API_KEY_GOERLI
-         );
+  // Goerli Alchemy
+  // const provider = new ethers.providers.AlchemyProvider("goerli",
+  //            process.env.ALCHEMY_API_KEY_GOERLI
+  //        );
+  const provider = new ethers.providers.InfuraProvider(
+      "goerli",
+      process.env.INFURA_API_KEY
+  );
 
 
   //console.log({ provider });
@@ -54,31 +58,60 @@ async function main() {
   // const safeFactory = await SafeFactory.create({ ethAdapter, isL1SafeMasterCopy: true })
   // const safeSdk = await Safe.create({ ethAdapter, safeAddress, isL1SafeMasterCopy: true })
   
+  
   // Deploy a Safe 
 
-  const safeFactory = await SafeFactory.create({ ethAdapter })
+  // const safeFactory = await SafeFactory.create({ ethAdapter })
 
-  const owners = ['0x934a406B7CAB0D8cB3aD201f0cdcA6a7855F43b0',
-      '0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209',
-      '0xD64258a33E7AC0294a9fdE8e4C9A76674bD33A23']
-  const threshold = 2
-  const safeAccountConfig: SafeAccountConfig = {
-      owners,
-      threshold
-      // ...
-  }
+  // const owners = ['0x934a406B7CAB0D8cB3aD201f0cdcA6a7855F43b0',
+  //     '0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209',
+  //     '0x801c2Ddcb5220f6B33Ac84D933176BeE478E4e78']
+  // const threshold = 2
+  // const safeAccountConfig: SafeAccountConfig = {
+  //     owners,
+  //     threshold
+  //     // ...
+  // }
 
-  const safeSdk: Safe = await safeFactory.deploySafe({ safeAccountConfig })
+  // const safeSdk: Safe = await safeFactory.deploySafe({ safeAccountConfig })
+
+  // Get the Safe Address
+  // const newSafeAddress = await safeSdk.getAddress()
+  // console.log({ newSafeAddress });
   
-  //const safeAccountConfig: SafeAccountConfig = {
-  //    owners: ['0x934a406B7CAB0D8cB3aD201f0cdcA6a7855F43b0',
-  //        '0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209',
-  //        '0xD64258a33E7AC0294a9fdE8e4C9A76674bD33A23'],
-  //    threshold: 2,
-  //    // ... (optional params)
-  //}
-  //const safeSdk = await safeFactory.deploySafe({ safeAccountConfig })
-    
+  // Connect to an Already Deployed Safe at newSafeAddress
+
+   const safeAddress = '0x524dc2CDd09D879678f4aC165C1707c697A789a7'
+   const safeSdk: Safe = await Safe.create({ ethAdapter: ethAdapter, safeAddress })
+
+   // Get the Safe Address
+   const newSafeAddress = await safeSdk.getAddress()
+   console.log({ newSafeAddress });
+
+   // Test Getting Some Safe Data
+
+   console.log(`the Safe Address is: (${newSafeAddress})\n`);
+   const ownerAddresses = await safeSdk.getOwners()
+   console.log(`Owners Addresses are: (${ownerAddresses})\n`);
+   const threshold = await safeSdk.getThreshold()
+   console.log(`the threshold is: (${threshold})\n`);
+   const balance = await safeSdk.getBalance()
+   console.log(`the Safe Balance is: (${balance})\n`);
+   const guardAddress = await safeSdk.getGuard()
+   console.log(`the guardAddress is: (${guardAddress})\n`);
+   const moduleAddresses = await safeSdk.getModules()
+   console.log(`the Modules enabled are: (${moduleAddresses})\n`);
+   const isOwner = await safeSdk.isOwner(safeOwner.address) 
+   console.log(`is the safeOwner an Owner?: (${isOwner})\n`);
+
+   // Deposit to the safe
+   
+   // await safeOwner.sendTransaction({
+   //     to: newSafeAddress,
+   //     value: ethers.utils.parseEther("0.001"), // Sends exactly 1.0 ether
+   // }) 
+
+
 }
 
 main().catch((error) => {
